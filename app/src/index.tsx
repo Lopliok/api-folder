c390a9b2c2ac330110b663835485270571b18ecb
mzt <lopliok@gmail.com>
2024-11-06 12:31:56 +0100

    fe: switch to router

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,23 @@
 import { createRoot } from "react-dom/client"
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import App from "./App"
+import React from "react"
+
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <App />,
+    },
+]);
 
 const container = document.getElementById("app")!
 const root = createRoot(container)
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
