94e1da707a3b44d35ad5a614879b835a0190ce79
mzt <lopliok@gmail.com>
2024-11-06 12:31:56 +0100

    fe: switch to router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import React, { ReactNode, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BsCameraVideo, BsFileMusic, BsFileText, BsFolder, BsCardImage, BsFileEarmarkPdf, BsFiletypeJson } from "react-icons/bs";
 import { FaJs } from "react-icons/fa";
 import { AiOutlineFileUnknown } from "react-icons/ai";
 import { SiTypescript } from "react-icons/si";
 import './App.css'
 import { download } from './utils';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const API_URL = process.env.API_URL ?? 'http://localhost:3000/'
+const API_URL = process.env.API_URL ?? 'http://localhost:3000'
 
 type Item = {
   name: string
@@ -39,15 +40,16 @@ const Icon = ({ type }: { type: string }) => {
 
 function App() {
   const [data, setData] = useState<Item[]>([])
-  const [path, setPath] = useState<string[]>([])
 
-  const buildFullPath = () => path.join("/")
+  const { pathname } = useLocation()
+
+  const navigate = useNavigate()
 
 
   const fetchData = async (filename?: string) => {
 
     try {
-      const response = await fetch(API_URL + buildFullPath() + (filename ? "/" + filename : ""), { method: "POST" })
+      const response = await fetch(API_URL + pathname + (filename ? "/" + filename : ""), { method: "POST" })
 
       if (filename) {
         response.blob().then(blob => download(blob, filename))
@@ -62,11 +64,17 @@ function App() {
 
   useEffect(() => {
     fetchData()
-  }, [path])
+  }, [pathname])
 
   const onItemClick = (item: Item) => {
-    item.type === "directory" && setPath(prev => [...prev, item.name])
+    const currentPath = pathname !== "/" ? pathname + "/" : ""
+    item.type === "directory" && navigate(currentPath + item.name)  //setPath(prev => [...prev, item.name])
     downloadableFiles.includes(item.type) && fetchData(item.name)
+  }
+
+  const goToParent = () => {
+    const path = pathname.split("/").slice(0, -1).join("/")
+    navigate(path)
   }
 
   return (
@@ -75,16 +83,16 @@ function App() {
         <h2 className="text-5xl font-italic underline text-grey-400">
           File explorer
         </h2>
-        <div className='p-4 ml-10'>Current path: <span className='font-bold'>{buildFullPath()}</span></div>
+        <div className='p-4 ml-10'>Current path: <span className='font-bold'>{pathname}</span></div>
       </div>
 
       <div className="p-3 m-1 text-sm w-full h-5/6 border border-gray-700 overflow-auto">
-        {path.length > 0 && <div className="flex p-2 hover:bg-yellow-200" onClick={() => setPath(p => p.slice(0, -1))}>
+        {pathname.length > 1 && <div className="flex p-2 hover:bg-yellow-200" onClick={goToParent}>
           <BsFolder className='mr-2 text-lg' />
           <div>..</div>
         </div>}
         {data.map((i) => (
-          <div className={`flex p-2 hover:bg-yellow-200 ${i.type === "directory" && "text-green-700"}`} onClick={() => onItemClick(i)}>
+          <div key={i.name} className={`flex p-2 hover:bg-yellow-200 ${i.type === "directory" && "text-green-700"}`} onClick={() => onItemClick(i)}>
             <Icon type={i.type} />
             <div>{i.name}</div>
           </div>
